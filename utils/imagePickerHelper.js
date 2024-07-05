7751ceeb991b1cf82b4e806b2037b8ef502a0bf5
muhammed rifkhan <rifkhanmuhammed17@gmail.com>
2024-07-05 19:49:28 +0530

    done image picker-1

--- utils/imagePickerHelper.js
+++ utils/imagePickerHelper.js
@@ -1,60 +1,72 @@
-import * as ImagePicker from 'expo-image-picker';
-import { Platform } from 'react-native';
-import { getFirebaseApp } from './firebaseHelper';
-import uuid from 'react-native-uuid';
-import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import * as ImagePicker from 'expo-image-picker'
+import { Platform } from 'react-native'
+import { getFirebaseApp } from './firebaseHelper'
+import uuid from 'react-native-uuid'
+import {
+	getDownloadURL,
+	getStorage,
+	ref,
+	uploadBytesResumable
+} from 'firebase/storage'
 
 export const launchImagePicker = async () => {
-    await checkMediaPermissions();
-    
-    const result = await ImagePicker.launchImageLibraryAsync({
-        mediaTypes: ImagePicker.MediaTypeOptions.Images,
-        allowsEditing: true,
-        aspect: [1, 1],
-        quality: 1
-    });
+	await checkMediaPermissions()
 
-    if (!result.cancelled) {
-        return result.uri;
-    }
+	const result = await ImagePicker.launchImageLibraryAsync({
+		mediaTypes: ImagePicker.MediaTypeOptions.Images,
+		allowsEditing: true,
+		aspect: [1, 1],
+		quality: 1
+	})
+
+	if (!result.canceled) {
+		return result.assets[0].uri
+	}
 }
 
-export const uploadImageAsync = async (uri) => {
-    const app = getFirebaseApp();
+export const uploadImageAsync = async uri => {
+	const app = getFirebaseApp()
 
-    const blob = await new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest();
-        xhr.onload = function () {
-            resolve(xhr.response);
-        };
+	try {
+		const blob = await new Promise((resolve, reject) => {
+			const xhr = new XMLHttpRequest()
+			xhr.onload = function () {
+				resolve(xhr.response)
+			}
 
-        xhr.onerror = function(e) {
-            console.log(e);
-            reject(new TypeError("Network request failed"));
-        };
+			xhr.onerror = function (e) {
+				console.log(e)
+				reject(new TypeError('Network request failed'))
+			}
 
-        xhr.responseType = "blob";
-        xhr.open("GET", uri, true);
-        xhr.send();
-    });
+			xhr.responseType = 'blob'
 
-    const pathFolder = 'profilePics';
-    const storageRef = ref(getStorage(app), `${pathFolder}/${uuid.v4()}`);
+			xhr.open('GET', uri, true)
 
-    await uploadBytesResumable(storageRef, blob);
+			console.log(uri)
+			xhr.send()
+		})
 
-    blob.close();
+		const pathFolder = 'profilePics'
+		const storageRef = ref(getStorage(app), `${pathFolder}/${uuid.v4()}`)
 
-    return await getDownloadURL(storageRef);
+		await uploadBytesResumable(storageRef, blob)
+
+		blob.close()
+		return await getDownloadURL(storageRef)
+	} catch (error) {
+		console.log(error)
+	}
 }
 
 const checkMediaPermissions = async () => {
-    if (Platform.OS !== 'web') {
-        const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
-        if (permissionResult.granted === false) {
-            return Promise.reject("We need permission to access your photos");
-        }
-    }
+	if (Platform.OS !== 'web') {
+		const permissionResult =
+			await ImagePicker.requestMediaLibraryPermissionsAsync()
+		if (permissionResult.granted === false) {
+			return Promise.reject('We need permission to access your photos')
+		}
+	}
 
-    return Promise.resolve();
+	return Promise.resolve()
 }
