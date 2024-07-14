c87ab37b1eb3400ca6146abc6c34db3fe031b1fd
muhammed rifkhan <rifkhanmuhammed17@gmail.com>
2024-07-14 13:45:08 +0530

    replyinf messages

--- utils/actions/chatActions.js
+++ utils/actions/chatActions.js
@@ -1,4 +1,4 @@
-import { child, getDatabase, push, ref, update } from "firebase/database";
+import { child, get, getDatabase, push, ref, remove, set, update } from "firebase/database";
 import { getFirebaseApp } from "../firebaseHelper";
 
 export const createChat = async (loggedInUserId, chatData) => {
@@ -24,7 +24,7 @@ export const createChat = async (loggedInUserId, chatData) => {
     return newChat.key;
 }
 
-export const sendTextMessage = async (chatId, senderId, messageText) => {
+export const sendTextMessage = async (chatId, senderId, messageText, replyTo) => {
     const app = getFirebaseApp();
     const dbRef = ref(getDatabase());
     const messagesRef = child(dbRef, `messages/${chatId}`);
@@ -35,6 +35,10 @@ export const sendTextMessage = async (chatId, senderId, messageText) => {
         text: messageText
     };
 
+    if (replyTo) {
+        messageData.replyTo = replyTo;
+    }
+
     await push(messagesRef, messageData);
 
     const chatRef = child(dbRef, `chats/${chatId}`);
@@ -43,4 +47,31 @@ export const sendTextMessage = async (chatId, senderId, messageText) => {
         updatedAt: new Date().toISOString(),
         latestMessageText: messageText
     });
+}
+
+export const starMessage = async (messageId, chatId, userId) => {
+    try {
+        const app = getFirebaseApp();
+        const dbRef = ref(getDatabase(app));
+        const childRef = child(dbRef, `userStarredMessages/${userId}/${chatId}/${messageId}`);
+
+        const snapshot = await get(childRef);
+
+        if (snapshot.exists()) {
+            // Starred item exists - Un-star
+            await remove(childRef);
+        }
+        else {
+            // Starred item does not exist - star
+            const starredMessageData = {
+                messageId,
+                chatId,
+                starredAt: new Date().toISOString()
+            }
+
+            await set(childRef, starredMessageData);
+        }
+    } catch (error) {
+        console.log(error);        
+    }
 }
